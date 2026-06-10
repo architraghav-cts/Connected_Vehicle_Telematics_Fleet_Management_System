b0ff70679737f9e319afd759e3ff2602e3baf7e6
Aman Kumar <2505361@cognizant.com>
2026-06-10 14:24:55 +0530

    created AuthService

--- backend/src/main/java/com/example/telematics_fleet_management/repository/UserRepository.java
+++ backend/src/main/java/com/example/telematics_fleet_management/repository/UserRepository.java
@@ -7,4 +7,6 @@ import com.example.telematics_fleet_management.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    User findByEmail(String email);
+
 }
